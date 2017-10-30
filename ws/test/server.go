6149d6e78abd0336bcbc4503e26ede05d755e47c
Nate Woods <big.nate.w@gmail.com>
2017-10-30 07:44:58 -0600

    Moving main back to server to reduce deps and fork problems

--- ws/test/server.go
+++ ws/test/server.go
@@ -1,19 +1,21 @@
-// +build ignore
-
 package main
+
+//go:generate gopherjs build -mv client.go
 
 import (
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/exec"
 	"time"
 
 	"github.com/gorilla/websocket"
 )
 
 var (
-	port     = flag.Int("port", 3001, "port to serve on")
+	port     = flag.Int("port", 3000, "port to serve on")
 	upgrader = websocket.Upgrader{
 		ReadBufferSize:  1024,
 		WriteBufferSize: 1024,
@@ -29,6 +31,52 @@ func check(err error, name string) {
 
 func main() {
 	flag.Parse()
+	updates := make(chan time.Time)
+	register := make(chan chan time.Time)
+	unregister := make(chan chan time.Time)
+
+	// Statis serve everything!
+	dir := os.Getenv("GOPATH") + "/src/github.com/bign8/msg/ws/test"
+	http.Handle("/", http.FileServer(http.Dir(dir)))
+
+	// Watch client
+	go watch("client.go", updates, func() error {
+		cmd := exec.Command("go", "generate", "./...")
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		return cmd.Run()
+	})
+
+	// Signal to all sockets
+	go func() {
+		listeners := make(map[chan<- time.Time]bool)
+		for {
+			select {
+			case client := <-register:
+				listeners[client] = true
+			case client := <-unregister:
+				delete(listeners, client)
+			case now := <-updates:
+				for client := range listeners {
+					client <- now
+				}
+			}
+		}
+	}()
+
+	// Watcher socket
+	http.HandleFunc("/ws/watcher", func(w http.ResponseWriter, r *http.Request) {
+		conn, _ := upgrader.Upgrade(w, r, nil)
+		ticker := make(chan time.Time)
+		register <- ticker
+		defer close(ticker)
+		for now := range ticker {
+			if conn.WriteJSON(now) != nil {
+				break
+			}
+		}
+		conn.Close()
+	})
 
 	// Socket to test against
 	http.HandleFunc("/ws/", func(w http.ResponseWriter, r *http.Request) {
@@ -54,4 +102,23 @@ func main() {
 
 	log.Printf("Server.go: Listening on :%d\n", *port)
 	check(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil), "ListenAndServe")
+}
+
+func watch(fn string, signal chan<- time.Time, onchange func() error) {
+	log.Printf("Watcher: %q", fn)
+	full := os.Getenv("GOPATH") + "/src/github.com/bign8/msg/ws/test/" + fn
+	var last time.Time
+	ticker := time.NewTicker(500 * time.Millisecond)
+	defer ticker.Stop()
+	for range ticker.C {
+		info, err := os.Stat(full)
+		check(err, "os.Stat")
+		if mod := info.ModTime(); last != mod {
+			log.Printf("%s: Starting Rebuild...", fn)
+			check(onchange(), "onchange")
+			log.Printf("%s: Rebuild Complete.", fn)
+			last = mod
+			signal <- mod
+		}
+	}
 }
