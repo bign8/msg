83140b9f78b3aeaed4b160636e2f9d0323edfcbb
Nate Woods <big.nate.w@gmail.com>
2017-10-30 08:07:30 -0600

    Using sync locker instead of home grown broadcast

--- ws/test/server.go
+++ ws/test/server.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"sync"
 	"time"
 
 	"github.com/gorilla/websocket"
@@ -31,47 +32,43 @@ func check(err error, name string) {
 
 func main() {
 	flag.Parse()
-	updates := make(chan time.Time)
-	register := make(chan chan time.Time)
-	unregister := make(chan chan time.Time)
+
+	// Allow broadcasting to multiple routines at once (not really protecting something)
+	cond := sync.NewCond((&sync.RWMutex{}).RLocker())
 
 	// Statis serve everything!
 	dir := os.Getenv("GOPATH") + "/src/github.com/bign8/msg/ws/test"
 	http.Handle("/", http.FileServer(http.Dir(dir)))
 
 	// Watch client
-	go watch("client.go", updates, func() error {
-		cmd := exec.Command("go", "generate", "./...")
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		return cmd.Run()
-	})
-
-	// Signal to all sockets
 	go func() {
-		listeners := make(map[chan<- time.Time]bool)
-		for {
-			select {
-			case client := <-register:
-				listeners[client] = true
-			case client := <-unregister:
-				delete(listeners, client)
-			case now := <-updates:
-				for client := range listeners {
-					client <- now
-				}
+		var last time.Time
+		ticker := time.NewTicker(500 * time.Millisecond)
+		defer ticker.Stop()
+		for range ticker.C {
+			info, err := os.Stat(dir + "/client.go")
+			check(err, "os.Stat")
+			if mod := info.ModTime(); last != mod {
+				log.Print("Starting Rebuild...")
+				cmd := exec.Command("go", "generate", "./...")
+				cmd.Stdout, cmd.Stderr = os.Stdout, os.Stderr
+				check(cmd.Run(), "onchange")
+				log.Print("Rebuild Complete.")
+				last = mod
+				cond.Broadcast()
 			}
 		}
 	}()
 
 	// Watcher socket
 	http.HandleFunc("/ws/watcher", func(w http.ResponseWriter, r *http.Request) {
-		conn, _ := upgrader.Upgrade(w, r, nil)
-		ticker := make(chan time.Time)
-		register <- ticker
-		defer close(ticker)
-		for now := range ticker {
-			if conn.WriteJSON(now) != nil {
+		conn, err := upgrader.Upgrade(w, r, nil)
+		check(err, "upgrader.Upgrade")
+		for {
+			cond.L.Lock()
+			cond.Wait() // https://golang.org/pkg/sync/#Cond.Wait
+			cond.L.Unlock()
+			if conn.WriteJSON(time.Now()) != nil {
 				break
 			}
 		}
@@ -93,6 +90,9 @@ func main() {
 		default: // echo server
 			for {
 				typ, p, err := conn.ReadMessage()
+				if websocket.IsCloseError(err, websocket.CloseNormalClosure, websocket.CloseGoingAway) {
+					return
+				}
 				check(err, "conn.ReadMessage")
 				check(conn.WriteMessage(typ, p), "conn.WriteMessage")
 			}
@@ -100,25 +100,6 @@ func main() {
 		conn.Close()
 	})
 
-	log.Printf("Server.go: Listening on :%d\n", *port)
+	log.Printf("Listening on :%d\n", *port)
 	check(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil), "ListenAndServe")
-}
-
-func watch(fn string, signal chan<- time.Time, onchange func() error) {
-	log.Printf("Watcher: %q", fn)
-	full := os.Getenv("GOPATH") + "/src/github.com/bign8/msg/ws/test/" + fn
-	var last time.Time
-	ticker := time.NewTicker(500 * time.Millisecond)
-	defer ticker.Stop()
-	for range ticker.C {
-		info, err := os.Stat(full)
-		check(err, "os.Stat")
-		if mod := info.ModTime(); last != mod {
-			log.Printf("%s: Starting Rebuild...", fn)
-			check(onchange(), "onchange")
-			log.Printf("%s: Rebuild Complete.", fn)
-			last = mod
-			signal <- mod
-		}
-	}
 }
