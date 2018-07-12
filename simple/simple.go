4903b6617eff58e3e92b4675a494bc76b70640de
Nate Woods <big.nate.w@gmail.com>
2018-07-11 23:53:16 -0600

    Furthering detail for simple messaging

--- simple/simple.go
+++ simple/simple.go
@@ -10,7 +10,7 @@ import (
 // 
 // Usage:
 //   RPC: in != nil && out != nil
-//   PUB: in != nil && out != nil
+//   PUB: in != nil && out == nil
 //   SUB: in == nil && out != nil && (_, ok := out.(Subscription); ok)
 //
 // For RPCs, Call serializes all the bytes from `in` and transmits them to the server.
@@ -20,7 +20,15 @@ import (
 //
 // For SUB a subscription is constructed for a particular channel and is held open until the Call context is Cancled.
 func Call(ctx context.Context, service, method string, in io.Reader, out io.Writer) error {
-	return errors.New("TODO: make the dream happen")
+	switch {
+		case in != nil && out != nil:
+			return rpc(ctx, service, method, in, out)
+		case in != nil && out == nil:
+			return pub(ctx, service, method, in)
+		case s, ok := out.(Subscription); in == nil && ok:
+			return sub(ctx, service, method, s)
+	}
+	return errors.New("simple.Call: un-supported argument configuration")
 }
 
 // Subscription is an interface that must be implemented when subscribing to streams.
@@ -31,3 +39,16 @@ type Subscription struct {
   // This should clear the current object frame and prepare for the next incomming message.
 	Reset() error
 }
+
+func rpc(ctx context.Context, service, method string, in io.Reader, out io.Writer) error {
+	return errors.New("simple.RPC: TODO")
+}
+
+func pub(ctx context.Context, service, method string, in io.Reader) error {
+	return errors.New("simple.PUB: TODO")
+}
+
+func sub(ctx context.Context, service, method string, s Subscription) error {
+	return errors.New("simple.SUB: TODO")
+}
+
