0b874bf77a5296a099f211a3264251572167b9a9
Nate Woods <big.nate.w@gmail.com>
2018-12-04 22:52:40 -0700

    Abstracting context to separate file

--- call.go
+++ call.go
@@ -2,17 +2,6 @@ package msg
 
 import "errors"
 
-type contextKey int
-
-const (
-	contextKeyCaller = contextKey(iota) // Caller
-)
-
-// WithCaller attaches various things to a given context
-func WithCaller(ctx Context, val Caller) Context {
-	return WithValue(ctx, contextKeyCaller, val)
-}
-
 // Reader is a copy of io.Reader.
 type Reader interface {
 	Read(p []byte) (n int, err error)
@@ -21,36 +10,6 @@ type Reader interface {
 // Writer is a copy of io.Writer.
 type Writer interface {
 	Write(p []byte) (n int, err error)
-}
-
-// Context is a copy of context.Context but only the methods that are needed.
-type Context interface {
-	// Deadline removed because time.Time doesn't transpile well.
-	Done() <-chan struct{}
-	Err() error
-	Value(key interface{}) interface{}
-}
-
-// WithValue is a copy of context.Context (with reflect Comparable check removed).
-func WithValue(parent Context, key, val interface{}) Context {
-	if key == nil {
-		panic("nil key")
-	}
-	return &valueCtx{parent, key, val}
-}
-
-// A valueCtx carries a key-value pair. It implements Value for that key and
-// delegates all other calls to the embedded Context.
-type valueCtx struct {
-	Context
-	key, val interface{}
-}
-
-func (c *valueCtx) Value(key interface{}) interface{} {
-	if c.key == key {
-		return c.val
-	}
-	return c.Context.Value(key)
 }
 
 // Caller is the base call type
---
+++ context.go
@@ -0,0 +1,42 @@
+package msg
+
+// Context is a copy of context.Context but only the methods that are needed.
+type Context interface {
+	// Deadline removed because time.Time doesn't transpile well.
+	Done() <-chan struct{}
+	Err() error
+	Value(key interface{}) interface{}
+}
+
+type contextKey int
+
+const (
+	contextKeyCaller = contextKey(iota) // Caller
+)
+
+// WithCaller attaches various things to a given context
+func WithCaller(ctx Context, val Caller) Context {
+	return WithValue(ctx, contextKeyCaller, val)
+}
+
+// WithValue is a copy of context.Context (with reflect Comparable check removed).
+func WithValue(parent Context, key, val interface{}) Context {
+	if key == nil {
+		panic("nil key")
+	}
+	return &valueCtx{parent, key, val}
+}
+
+// A valueCtx carries a key-value pair. It implements Value for that key and
+// delegates all other calls to the embedded Context.
+type valueCtx struct {
+	Context
+	key, val interface{}
+}
+
+func (c *valueCtx) Value(key interface{}) interface{} {
+	if c.key == key {
+		return c.val
+	}
+	return c.Context.Value(key)
+}
